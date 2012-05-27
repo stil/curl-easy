197c2348675e236bd9a1fc960c42ee3c982cb263
stil <przemekryciuk@gmail.com>
2012-05-27 19:05:53 +0200

    moved examples to README

--- examples/autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-set_include_path(get_include_path() . PATH_SEPARATOR . realpath(__DIR__.'/../library'));
-
-spl_autoload_register(function($class) {
-	require strtr($class,'\\','/') . '.php';
-});
