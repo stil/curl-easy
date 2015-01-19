1cb0853510462e054d79285eb790c86971146229
Lukas Kremsmayr <lukas@kremsmayr.net>
2015-01-19 11:43:43 +0100

    Update Event.php

--- src/cURL/Event.php
+++ src/cURL/Event.php
@@ -5,6 +5,6 @@ use Symfony\Component\EventDispatcher\Event as SymfonyEvent;
 
 class Event extends SymfonyEvent
 {
-  /** @var Response $response */
+  	/** @var Response $response */
 	public $response = null;
 }
